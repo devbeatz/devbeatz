8cdfafd99dfaa5d2cb65b9c95ad8c5474adabe55
User <user@Users-MacBook-Pro.local>
2019-09-02 13:11:08 -0500

    change component styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Homepage from "./components/Homepage/Homepage";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <Homepage />
     </div>
   );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,33 @@
 import React from "react";
+import githubIcon from "../../images/github.png";
 import "./Footer.scss";
 
 function Footer(props) {
   return (
     <div id="footer">
       <p>
-        Made by <span className="emphasis">Tabitha Sin</span>,{" "}
-        <span className="emphasis">Jack Lenihan</span>, and{" "}
-        <span className="emphasis">Nolan Harris</span>.
+        Made by{" "}
+        <span className="emphasis">
+          <a href="https://github.com/tkxx" target="_blank">
+            Tabitha Sin
+          </a>
+        </span>
+        ,{" "}
+        <span className="emphasis">
+          <a href="https://github.com/Jack-B-Lenihan" target="_blank">
+            Jack Lenihan
+          </a>
+        </span>
+        , and{" "}
+        <span className="emphasis">
+          <a href="https://github.com/nolanjharris" target="_blank">
+            Nolan Harris
+          </a>
+        </span>
+        .{" "}
+        <a href="https://github.com/devbeatz">
+          <img src={githubIcon} alt="github icon" id="github-icon" />
+        </a>
       </p>
     </div>
   );
