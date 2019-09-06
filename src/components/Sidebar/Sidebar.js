e8fd7d505dc03eaf559574cd5b527a235ae8ad1c
User <user@Users-MacBook-Pro.local>
2019-09-06 16:22:45 -0500

    dashboard and sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -16,6 +16,8 @@ function Sidebar(props) {
         <Link to="/">
           <h1>Beatz</h1>
         </Link>
+      </div>
+      <div id="dashboard-buttons-div">
         <Link to="/Dashboard">
           <button id="dashboard-button">Dashboard</button>
         </Link>
@@ -31,25 +33,25 @@ function Sidebar(props) {
             Login
           </button>
         )}
-        <div id="sidebar-search">
-          <Link to="/Browse">
-            <button>Browse All Beats</button>
-          </Link>
-          <div className="sidebar-titles">
-            <h3>Genres</h3>
-          </div>
-          <div id="genre-list">
-            <a href="/">Alternative</a>
-            <a href="/">Blues</a>
-            <a href="/">Freestyle</a>
-            <a href="/">Hip-Hop</a>
-            <a href="/">Old School</a>
-            <a href="/">Pop</a>
-            <a href="/">R&B</a>
-            <a href="/">Soul</a>
-            <a href="/">Trap</a>
-            <a href="/">Underground</a>
-          </div>
+      </div>
+      <div id="sidebar-search">
+        <Link to="/Browse">
+          <button>Browse All Beats</button>
+        </Link>
+        <div className="sidebar-titles">
+          <h3>Genres</h3>
+        </div>
+        <div id="genre-list">
+          <a href="/">Alternative</a>
+          <a href="/">Blues</a>
+          <a href="/">Freestyle</a>
+          <a href="/">Hip-Hop</a>
+          <a href="/">Old School</a>
+          <a href="/">Pop</a>
+          <a href="/">R&B</a>
+          <a href="/">Soul</a>
+          <a href="/">Trap</a>
+          <a href="/">Underground</a>
         </div>
       </div>
     </div>
