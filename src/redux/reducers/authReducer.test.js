aa826fbfadd5eb82e00fd7eb0e07015f312fa4fc
User <user@Users-MacBook-Pro.local>
2019-09-10 15:56:48 -0500

    pass tests

--- src/redux/reducers/authReducer.test.js
+++ src/redux/reducers/authReducer.test.js
@@ -1,22 +1,4 @@
 import {
-  GET_ALL_TRACKS,
-  getAllTracks,
-  RESET_GENRE,
-  resetGenre,
-  GET_TOP_5_TRACKS,
-  getTopFiveTracks,
-  GET_TRACKS_BY_GENRE,
-  getTracksByGenre,
-  GET_TRACKS_BY_USER,
-  getTracksByUser,
-  ADD_TRACK,
-  addTrack,
-  DELETE_TRACK,
-  deleteTrack,
-  UPDATE_TRACK,
-  updateTrack
-} from "./trackReducer";
-import {
   LOGIN_USER,
   loginUser,
   REGISTER_USER,
@@ -34,14 +16,56 @@ import axios from "axios";
 import * as actions from "./authReducer";
 import * as types from "./authReducer";
 
-describe("registerNewUser", () => {
+describe("registerUser", () => {
   let userInput = "lilDucky";
   it("should bring in payload", () => {
-    const text = "payload";
     const expectedAction = {
       type: types.REGISTER_USER,
-      payload: axios.post("/auth/register", $`{userInput}`)
+      payload: axios.post(`/auth/register, ${userInput}`)
     };
-    expectedAction(actions.registerNewUser(userInput)).toEqual(expectedAction);
+    expect(actions.registerUser(userInput)).toEqual(expectedAction);
+  });
+});
+
+test("input with chucky", () => {
+  expect(registerUser("chucky")).toEqual({
+    type: REGISTER_USER,
+    payload: axios.post(`auth/register`)
+  });
+});
+
+describe("loginUser", () => {
+  let userInput = "NoLuckJames";
+  it("should bring in payload", () => {
+    const expectedAction = {
+      type: types.LOGIN_USER,
+      payload: axios.post(`/auth/login, ${userInput}`)
+    };
+    expect(actions.loginUser(userInput)).toEqual(expectedAction);
+  });
+});
+
+test("input with hammertime", () => {
+  expect(loginUser("hammertime")).toEqual({
+    type: LOGIN_USER,
+    payload: axios.post(`auth/login`)
+  });
+});
+
+describe("logoutUser", () => {
+  it("should bring in payload", () => {
+    let userInput = "NoLuckJames";
+    const expectedAction = {
+      type: types.LOGOUT_USER,
+      payload: axios.get("/auth/logout")
+    };
+    expect(actions.logoutUser(userInput)).toEqual(expectedAction);
+  });
+});
+
+test("input with ricoNasty", () => {
+  expect(logoutUser("ricoNasty")).toEqual({
+    type: LOGOUT_USER,
+    payload: axios.get("/auth/logout")
   });
 });
