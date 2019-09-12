2b1811a1dad12e85add68e2130d2eafb12298b52
nolanjharris <thenolanjames@gmail.com>
2019-09-12 14:10:31 -0500

    update readme

--- src/components/PurchaseBeat/StripeForm.js
+++ src/components/PurchaseBeat/StripeForm.js
@@ -1,60 +1,68 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import {CardElement, injectStripe, ReactStripeElements} from 'react-stripe-elements';
+import React, { useState } from "react";
+import axios from "axios";
+import {
+  CardElement,
+  injectStripe,
+  ReactStripeElements
+} from "react-stripe-elements";
 
-function StripeForm(props){
-    const [ username, setUsername ] = useState('');
-    const [ Amount, setAmount ] = useState(props.base);
-    const [ exclusive, toggleExclusive ] = useState(false)
+function StripeForm(props) {
+  const [username, setUsername] = useState("");
+  const [Amount, setAmount] = useState(props.base);
+  const [exclusive, toggleExclusive] = useState(false);
 
-    const submitHandler = async () => {
-        const { user_id } = props
-        props.onHide();
-        try{
-            let amount = Number(Amount.slice(1))
-            let { token } = await props.stripe.createToken({name: username, id: user_id})
-            console.log(token) 
-            await fetch('/api/buy',{
-                method: 'POST',
-                headers: {
-                    'content-type': 'application/json'
-                },
-                body: JSON.stringify({token, amount})
-            })
-        }catch (e){
-            throw e;
-            console.log(e)
-        }
-        const { track_id } = props;
-        console.log(track_id);
-        axios.post('/api/purchases/make', { track_id, exclusive })
+  const submitHandler = async () => {
+    const { user_id } = props;
+    props.onHide();
+    try {
+      let amount = Number(Amount.slice(1));
+      let { token } = await props.stripe.createToken({
+        name: username,
+        id: user_id
+      });
+      console.log(token);
+      await fetch("/api/buy", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify({ token, amount })
+      });
+    } catch (e) {
+      throw e;
+      console.log(e);
     }
+    const { track_id } = props;
+    console.log(track_id);
+    axios.post("/api/purchases/make", { track_id, exclusive });
+  };
 
-    return(
-        <form onSubmit={submitHandler}>
-            <label>Name</label>
-            <input 
-                value={username}
-                onChange={e => setUsername(e.target.value)}></input>
+  return (
+    <form onSubmit={submitHandler}>
+      <label>Name</label>
+      <input
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+      ></input>
 
-            <label>Exclusive: {props.exclusive}</label>
-            <input
-                value={exclusive}
-                onChange={e => {
-                    toggleExclusive(true);
-                    setAmount(props.exclusive);
-                }}
-                type='checkbox'/>
+      <label>Exclusive: {props.exclusive}</label>
+      <input
+        value={exclusive}
+        onChange={e => {
+          toggleExclusive(true);
+          setAmount(props.exclusive);
+        }}
+        type="checkbox"
+      />
 
-            <h3>Total: {Amount}</h3>
+      <h3>Total: {Amount}</h3>
 
-            <CardElement />
+      <CardElement />
 
-            <a href={props.trackUrl} download>
-                <button type='submit'>Confirm</button>
-            </a>
-                
-        </form>
-    )
+      <a href={props.trackUrl} download>
+        <button type="submit">Confirm</button>
+      </a>
+    </form>
+  );
 }
 export default injectStripe(StripeForm);
